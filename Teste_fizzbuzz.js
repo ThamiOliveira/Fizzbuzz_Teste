5d92d095362f3599957ba1f34e5b00fa25f1afc7
Thamiris Oliveira <thamioliveira6@gmail.com>
2021-03-15 17:13:02 -0300

    criando funções

--- Teste_fizzbuzz.js
+++ Teste_fizzbuzz.js
@@ -1,17 +1,20 @@
-loop(); 
-function loop(){
-    
-for( i= 1; i <= 100; i++){
-    if(i % 3 == 0)
-    console.log("Fizz")
-
-    if(i % 5 == 0)
-    console.log("Buzz")
-
-    if(i % 3 == 0 && i % 5 ==0)
-    console.log("Fizzbuzz")
-    
-    if(i % 3 != 0 && i % 5 != 0)
-    console.log(i)
+FizzBuzz(); 
+function FizzBuzz(){
+    VerificaNumeros();
 }
+
+function VerificaNumeros(){
+    for( i= 1; i <= 100; i++){
+        if(i % 3 == 0)
+        console.log("Fizz")
+    
+        if(i % 5 == 0)
+        console.log("Buzz")
+    
+        if(i % 3 == 0 && i % 5 ==0)
+        console.log("Fizzbuzz")
+        
+        if(i % 3 != 0 && i % 5 != 0)
+        console.log(i)
+    }
 }
